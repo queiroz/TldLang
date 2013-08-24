86a14e7934c29fa1a145266136d88bf5caa875a3
Rodrigo Queiroz <rodrigo@queiroz.org>
2013-08-24 14:32:49 +0100

    initial package

---
+++ src/Queiroz/TldLang/TldLangServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace Queiroz\TldLang;
+
+use Illuminate\Support\ServiceProvider;
+
+class TldLangServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('queiroz/tld-lang');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
